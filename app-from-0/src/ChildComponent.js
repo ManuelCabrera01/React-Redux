cb3e30f85631126bfdd1e7cfddae4fb6d9a2cf97
Manuel <m.a.c.p90@gmail.com>
2018-09-23 10:48:13 -0400

    stateless component

--- app-from-0/src/ChildComponent.js
+++ app-from-0/src/ChildComponent.js
@@ -1,21 +1,17 @@
 import React, { Component } from "react";
 
-class ChildComponent extends Component {
-  render() {
-    // console.log(this.props);
-    const { clients } = this.props;
-    const clientList = clients.map(client => {
-      return (
-        <div className="ninjas" key={client.id}>
-          <p>name: {client.name}</p>
-          <p>age: {client.age}</p>
+const ChildComponent = clients => {
+  const clientList = clients.map(client => {
+    return (
+      <div className="ninjas" key={client.id}>
+        <p>name: {client.name}</p>
+        <p>age: {client.age}</p>
 
-          <br />
-        </div>
-      );
-    });
-    return <div className="client-list">{clientList}</div>;
-  }
-}
+        <br />
+      </div>
+    );
+  });
+  return <div className="client-list">{clientList}</div>;
+};
 
 export default ChildComponent;
