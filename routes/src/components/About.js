e0e7922dd782658cccd87017528e2bc4b40569a6
Manuel <m.a.c.p90@gmail.com>
2018-09-26 20:20:36 -0400

    home routes contacts componet created

---
+++ routes/src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est possimus
+        delectus, porro aspernatur voluptatum quae sapiente eos. Doloremque, et.
+        Quasi odit doloribus nesciunt nemo eveniet. Illum possimus explicabo
+        delectus debitis.
+      </p>
+    </div>
+  );
+};
+export default About;
---
+++ routes/src/components/Homes.js
@@ -0,0 +1,15 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est possimus
+        delectus, porro aspernatur voluptatum quae sapiente eos. Doloremque, et.
+        Quasi odit doloribus nesciunt nemo eveniet. Illum possimus explicabo
+        delectus debitis.
+      </p>
+    </div>
+  );
+};
+export default Home;
